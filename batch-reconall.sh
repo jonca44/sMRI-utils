f0e7f5dd5a1b0e9693a2d852670f602b03e1d038
Warren Winter <warren.winter@childrens.harvard.edu>
2013-04-30 06:27:53 -0300

    Update batch-reconall.sh

--- batch-reconall.sh
+++ batch-reconall.sh
@@ -1,88 +1,72 @@
 #!/bin/bash 
 #Batch processes multiple recon-all jobs, takes user input about where to start the pipeline
 #Note: Local custom settings:
-#	- Uses an xopts.txt file created in SUBJECTS_DIR
-#	- Uses a 2011 brain atlas that is not the default in FreeSurfer 5.0.
+#	- Uses 3T atlas for Talairach alignment, 3T-specific NU intensity correction parameters, and MPRAGE-specific intensity normalization parameters
+#	- Allows use of T2-weighted images for refinement of pial surfaces
+#	- Uses a GCA atlas (made in 2011) that is not default in FreeSurfer 5.2 (uses the 2008 version) but that is better for putamen segmentation
 #	- Disables labeling of white matter segmentation abnormalities
+#	- IMPORTANT: Change paths to the T1 and T2 DICOM files and to the GCA files
 #Author: Warren Winter
-#Date: 02/25/2013
+#Date: 04/26/2013
 
 
 ##########
 
-function reconall_position_select() {
-	echo '
-	Choose one of the following options:
+echo '
+Choose one of the following options:
+
+(1) recon-all from raw image data using just a T1,
+(2) recon-all from raw image data using a T1 and a T2,
+(3) recon-all after adding control points,
+(4) recon-all after editing brainmask.mgz because of a bad skull strip,
+(5) recon-all after editing wm.mgz, or
+(6) recon-all after editing brainmask.mgz because of a bad skull strip and wm.mgz as well.
 	
-	(1) recon-all from raw image data using just one T1 acquisition,
-	(2) recon-all after adding control points,
-	(3) recon-all after editing brainmask.mgz because of a bad skull strip,
-	(4) recon-all after editing wm.mgz, or
-	(5) recon-all after editing brainmask.mgz because of a bad skull strip and wm.mgz as well.
-	
-	This prompt will repeat after each subject is entered.  To close out of the prompt loop, hit Control+C.
-	
-	'
+This prompt will repeat after each subject is entered. To close out of the prompt loop, hit Control+C.
+
+'
+read choice
+
+if ! [[ $choice =~ ^[1-4]+$ ]]; then
+	echo "Not a valid option. Enter either 1, 2, 3, or 4"
 	read choice
-
-	if [ $choice -eq 1 ]; then 
-		echo "Enter subject ID.  " ; read subjid ; echo "Enter path to the subject's first .dcm or .nii file for his/her T1 MPRAGE RMS scan" ; read dcmpath ; echo "Starting a full recon-all for subject number $subjid" ; gnome-terminal --tab --title="recon-all; subjid: $subjid" -e "/bin/bash -c '. chb-fs stable-5.0 ; mosbatch recon-all -i $dcmpath -subjid $subjid ; cp xopts.txt ${subjid}/scripts/expert.opts ; mosbatch recon-all -expert ${subjid}/scripts/expert.opts -xopts-overwrite -s $subjid -mprage -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-	elif [ $choice -eq 2 ]; then
-		echo "Enter subject ID.  " ; read subjid ; echo "Starting recon-all from -autorecon2-cp to the end for subject number $subjid" ; gnome-terminal --tab --title="-autorecon2-cp -autorecon3; subjid: $subjid" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjid}/scripts/expert.opts ; mosbatch recon-all -expert ${subjid}/scripts/expert.opts -xopts-overwrite -s $subjid -autorecon2-cp -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-	elif [ $choice -eq 3 ]; then 
-		echo "Enter subject ID.  " ; read subjid ; echo "Starting recon-all from -autorecon2 to the end for subject number $subjid" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjid" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjid}/scripts/expert.opts ; mosbatch recon-all -expert ${subjid}/scripts/expert.opts -xopts-overwrite -s $subjid -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-	elif [ $choice -eq 4 ]; then 
-		echo "Enter subject ID.  " ; read subjid ; echo "Starting recon-all from -autorecon2-wm to the end for subject number $subjid" ; gnome-terminal --tab --title="-autorecon2-wm -autorecon3; subjid: $subjid" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjid}/scripts/expert.opts ; mosbatch recon-all -expert ${subjid}/scripts/expert.opts -xopts-overwrite -s $subjid -autorecon2-wm -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-	elif [ $choice -eq 5 ]; then 
-		echo "Enter subject ID.  " ; read subjid ; echo "Starting recon-all from -autorecon2 to the end for subject number $subjid" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjid" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjid}/scripts/expert.opts ; mosbatch recon-all -expert ${subjid}/scripts/expert.opts -xopts-overwrite -s $subjid -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-	fi
-}
-
-echo "Reprocess multiple subjects from the same point in the recon-all stream? (yes/no)"
-read multisubjs
-if [ $multisubjs == "yes" ] || [ $multisubjs == "Yes" ] || [ $multisubjs == "y" ] || [ $multisubjs == "Y" ]; then
-	echo '
-	Choose one of the following options:
-	
-	(1) recon-all after adding control points,
-	(2) recon-all after editing brainmask.mgz because of a bad skull strip,
-	(3) recon-all after editing wm.mgz, or
-	(4) recon-all after editing brainmask.mgz because of a bad skull strip and wm.mgz as well.
-	
-	This prompt will repeat after each subject is entered.  To close out of the prompt loop, hit Control+C.
-	
-	'
-	read choice
-
-	if ! [[ $choice =~ ^[1-4]+$ ]]; then
-		echo "Not a valid option.  Enter either 1, 2, 3, or 4"
-		read choice
-	fi
-
-	echo "List the subjects' subject IDs, separated by spaces:"
-	read subjsinput
-	
-	echo $subjsinput | tr ' ' '\n' > subjsinput_ra.txt
-	cat subjsinput_ra.txt | while read subjinput
-	do
-		if [ $choice -eq 1 ]; then 
-			echo "Starting recon-all from -autorecon2-cp to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-cp -autorecon3; subjinput: $subjinput" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjinput}/scripts/expert.opts ; mosbatch recon-all -expert ${subjinput}/scripts/expert.opts -xopts-overwrite -s $subjinput -autorecon2-cp -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-		elif [ $choice -eq 2 ]; then 
-			echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjinput: $subjinput" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjinput}/scripts/expert.opts ; mosbatch recon-all -expert ${subjinput}/scripts/expert.opts -xopts-overwrite -s $subjinput -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-		elif [ $choice -eq 3 ]; then 
-			echo "Starting recon-all from -autorecon2-wm to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-wm -autorecon3; subjinput: $subjinput" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjinput}/scripts/expert.opts ; mosbatch recon-all -expert ${subjinput}/scripts/expert.opts -xopts-overwrite -s $subjinput -autorecon2-wm -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-		elif [ $choice -eq 4 ]; then 
-			echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjinput: $subjinput" -e "/bin/bash -c '. chb-fs stable-5.0 ; cp xopts.txt ${subjinput}/scripts/expert.opts ; mosbatch recon-all -expert ${subjinput}/scripts/expert.opts -xopts-overwrite -s $subjinput -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /chb/sheridanlab/software/etc -nowmsa'"
-		fi
-	done
-	rm subjsinput_ra.txt
-else
-	a=1
-	while [ $a -gt 0 ]
-	do
-		subjid=
-		choice=
-		dcmpath=
-		reconall_position_select
-	done
 fi
+
+if [ $choice != 1 ] & [ $choice != 2 ] ; then
+	echo 'Using T2 image to refine pial surfaces? yes/no'
+	read T2pial
+	
+	if [ $T2pial != "yes" ] & [ $T2pial != "no" ]; then
+		echo "Not a valid option. Type either yes or no"
+		read T2pial
+	fi
+	
+	if [ $T2pial = "yes" ]; then
+		T2 = "-T2pial"
+	else
+		T2 = ""
+	fi
+fi
+
+echo "List the subjects' subject IDs, separated by spaces:"
+read subjsinput
+	
+echo $subjsinput | tr ' ' '\n' > subjsinput_ra.txt
+cat subjsinput_ra.txt | while read subjinput
+do
+	if [ $choice -eq 1 ]; then
+		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i /XXX/XXXXXX/XXXXXX/${subjinput}/*T1_MPRAGE_RMS/*-1.dcm -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+	elif [ $choice -eq 2 ]; then 
+		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i /XXX/XXXXXX/XXXXXX/${subjinput}/*T1_MPRAGE_RMS/*-1.dcm -T2 /XXX/XXXXXX/XXXXXX/${subjinput}/*T2_SPACE/*-1.dcm -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -T2pial -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+	elif [ $choice -eq 3 ]; then 
+		echo "Starting recon-all from -autorecon2-cp to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-cp -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-cp -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+	elif [ $choice -eq 4 ]; then 
+		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+	elif [ $choice -eq 5 ]; then 
+		echo "Starting recon-all from -autorecon2-wm to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-wm -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-wm -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+	elif [ $choice -eq 6 ]; then 
+		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
+fi
+done
+
+rm subjsinput_ra.txt
