4593131030d00d19745adc2e19d8fc7fd8bc4792
Warren Winter <warren.winter@childrens.harvard.edu>
2013-05-07 17:11:48 -0300

    User input validation

--- batch-reconall.sh
+++ batch-reconall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #Batch processes multiple recon-all jobs, takes user input about where to start the pipeline
 #Note: Local custom settings:
-#	- Uses 3T atlas for Talairach alignment, 3T-specific NU intensity correction parameters, and MPRAGE-specific intensity normalization parameters
+#	- Uses 3T-tailored talairach and intensity normalization tailored to MPRAGE scans
 #	- Allows use of T2-weighted images for refinement of pial surfaces
-#	- Uses a GCA atlas (made in 2011) that is not default in FreeSurfer 5.2 (uses the 2008 version) but that is better for putamen segmentation
+#	- Uses a 2011 brain atlas that is not the default in FreeSurfer 5.2
 #	- Disables labeling of white matter segmentation abnormalities
-#	- IMPORTANT: Change paths to the T1 and T2 DICOM files and to the GCA files
+#	- IMPORTANT: Assumes a directory structure where /XXX/XXXXXX/XXXXX/<subjid>/<subjid>_organized/*EllisonRMS/ contains the T1 DICOMs, and where /XXX/XXXXXX/XXXXX/<subjid>/<subjid>_organized/*T2_SPACE/ contains the T2 DICOMs
 #Author: Warren Winter
-#Date: 04/26/2013
+#Date: 05/07/2013
 
 
 ##########
@@ -27,46 +27,72 @@ This prompt will repeat after each subject is entered. To close out of the promp
 '
 read choice
 
-if ! [[ $choice =~ ^[1-4]+$ ]]; then
-	echo "Not a valid option. Enter either 1, 2, 3, or 4"
-	read choice
-fi
+while ! [[ $choice =~ ^[1-6]+$ ]] || [[ $choice -gt 6 ]] || [[ $choice -lt 1 ]]; do
+	read -p "Not a valid option.  Enter an integer from 1 to 6: " choice
+done
 
-if [ $choice != 1 ] & [ $choice != 2 ] ; then
-	echo 'Using T2 image to refine pial surfaces? yes/no'
-	read T2pial
-	
-	if [ $T2pial != "yes" ] & [ $T2pial != "no" ]; then
-		echo "Not a valid option. Type either yes or no"
-		read T2pial
-	fi
-	
-	if [ $T2pial = "yes" ]; then
-		T2 = "-T2pial"
+if [[ $choice -gt 2 ]]; then
+	read -p 'Using T2 image to refine pial surfaces? yes/no: ' T2pial
+	while [[ $T2pial != "yes" ]] & [[ $T2pial != "no" ]]; do
+		read -p "Not a valid option. Type either yes or no: " T2pial
+	done
+	if [[ $T2pial = "yes" ]]; then
+		T2="-T2pial"
 	else
-		T2 = ""
+		T2=""
 	fi
 fi
 
-echo "List the subjects' subject IDs, separated by spaces:"
-read subjsinput
+read -p "List the subjects' subject IDs, separated by spaces: " subjsinput
 	
 echo $subjsinput | tr ' ' '\n' > subjsinput_ra.txt
-cat subjsinput_ra.txt | while read subjinput
-do
-	if [ $choice -eq 1 ]; then
-		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i /XXX/XXXXXX/XXXXXX/${subjinput}/*T1_MPRAGE_RMS/*-1.dcm -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-	elif [ $choice -eq 2 ]; then 
-		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i /XXX/XXXXXX/XXXXXX/${subjinput}/*T1_MPRAGE_RMS/*-1.dcm -T2 /XXX/XXXXXX/XXXXXX/${subjinput}/*T2_SPACE/*-1.dcm -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -T2pial -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-	elif [ $choice -eq 3 ]; then 
-		echo "Starting recon-all from -autorecon2-cp to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-cp -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-cp -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-	elif [ $choice -eq 4 ]; then 
-		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-	elif [ $choice -eq 5 ]; then 
-		echo "Starting recon-all from -autorecon2-wm to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-wm -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-wm -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-	elif [ $choice -eq 6 ]; then 
-		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXXX/ -nowmsa'"
-fi
-done
+while read -u 3 subjinput; do
+	if [[ $choice -eq 1 ]]; then
+		numT1s=`ls /XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*EllisonRMS/*-1.dcm -1 | wc -l`
+		if [[ $numT1s -gt 1 ]]; then
+			read -p "More than one T1 acquisition series found.  Please enter series number of the acquisition you want to reconstruct (e.g., 15): " T1seriesnum
+			T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T1seriesnum}_*EllisonRMS/*-1.dcm"
+			while ! ls $T1series ; do
+				read -p "Acquisition series not found.  Re-enter series number: " T1seriesnum
+				T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T1seriesnum}_*EllisonRMS/*-1.dcm"
+			done
+		else
+			T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*EllisonRMS/*-1.dcm"
+		fi
+		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i $T1series -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	elif [[ $choice -eq 2 ]]; then 
+		numT1s=`ls /XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*EllisonRMS/*-1.dcm -1 | wc -l`
+		numT2s=`ls /XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*T2_SPACE/*-1.dcm -1 | wc -l`
+		if [[ $numT1s -gt 1 ]]; then
+			read -p "More than one T1 acquisition series found.  Please enter series number of the acquisition you want to reconstruct (e.g., 15): " T1seriesnum
+			T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T1seriesnum}_*EllisonRMS/*-1.dcm"
+			while ! ls $T1series ; do
+				read -p "Acquisition series not found.  Re-enter series number: " T1seriesnum
+				T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T1seriesnum}_*EllisonRMS/*-1.dcm"
+			done
+		else
+			T1series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*EllisonRMS/*-1.dcm"
+		fi
+		if [[ $numT2s -gt 1 ]] ; then
+			read -p "More than one T2 acquisition series found.  Please enter series number of the acquisition you want to reconstruct (e.g., 11): " T2seriesnum
+			T2series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T2seriesnum}_*T2_SPACE/*-1.dcm"
+			while ! ls $T2series ; do
+				read -p "Acquisition series not found.  Re-enter series number: " T2seriesnum
+				T2series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/${T2seriesnum}_*T2_SPACE/*-1.dcm"
+			done
+		else
+			T2series="/XXX/XXXXXX/XXXXX/${subjinput}/${subjinput}_organized/*T2_SPACE/*-1.dcm"
+		fi
+		echo "Starting a full recon-all for subject number $subjinput" ; gnome-terminal --tab --title="recon-all; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -i $T1series -T2 $T2series -subjid $subjinput ; mosbatch recon-all -s $subjinput -3T -mprage -T2pial -all -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	elif [[ $choice -eq 3 ]]; then 
+		echo "Starting recon-all from -autorecon2-cp to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-cp -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-cp -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	elif [[ $choice -eq 4 ]]; then 
+		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	elif [[ $choice -eq 5 ]]; then 
+		echo "Starting recon-all from -autorecon2-wm to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2-wm -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2-wm -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	elif [[ $choice -eq 6 ]]; then 
+		echo "Starting recon-all from -autorecon2 to the end for subject number $subjinput" ; gnome-terminal --tab --title="-autorecon2 -autorecon3; subjid: $subjinput" -e "/bin/bash -c '. chb-fs stable ; mosbatch recon-all -s $subjinput -3T -mprage $T2 -autorecon2 -autorecon3 -gca RB_all_2011-10-25.gca -gca-skull RB_all_withskull_2011-10-25.gca -gca-dir /XXX/XXXXXX/XXXXX -nowmsa'"
+	fi
+done 3< subjsinput_ra.txt
 
 rm subjsinput_ra.txt
